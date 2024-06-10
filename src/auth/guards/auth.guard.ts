f1fc70bcc5f6ef061d42115437ac32da42a49871
oscarbarritc <73568002+oscarbarritc@users.noreply.github.com>
2024-06-10 16:59:18 -0600

    primer comit

---
+++ src/auth/guards/auth.guard.ts
@@ -0,0 +1,50 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from '../interfaces/jwt-payload';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private jwtService: JwtService,
+    private authservice: AuthService,
+
+  ) {}
+  
+  async canActivate( context: ExecutionContext ): Promise<boolean> {
+
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException( 'No hay token en la peticion' );
+    }
+    
+    try {
+      const payload = await this.jwtService.verifyAsync<JwtPayload>(
+        token, { secret: process.env.JWT_SEED }
+      );
+
+      const user = await this.authservice.findUserById( payload.id );
+      if ( !user ) throw new UnauthorizedException( 'Usuario no existe' );
+      if ( !user.isActive ) throw new UnauthorizedException( 'Usuario no activo' );
+      
+      request['user'] = user;
+
+    } catch (error) {
+      throw new UnauthorizedException();
+
+    }
+    
+    
+    
+    return true ;
+  }
+
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers['authorization']?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+
+}
